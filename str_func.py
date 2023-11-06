6b28edd43c068db34b720983dfe8c03beadf4d9c
albertsalikhov <bert56@yandex.ru>
2023-11-06 22:10:25 +0300

    def func_2

--- str_func.py
+++ str_func.py
@@ -2,6 +2,12 @@ def func():
     line = str(input())
     return line.upper()
 
+def func_2():
+    """ делает заглавными первые буквы каждого слова в строке"""
+    line = str(input())
+    return line.title()
+
+
 
 
 
